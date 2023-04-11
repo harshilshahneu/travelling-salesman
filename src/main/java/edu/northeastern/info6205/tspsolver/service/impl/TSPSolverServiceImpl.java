8cba562a3b085b101432ae9ff4bd8f53d5a016a4
Harshil Shah <shah.harshil2@northeastern.edu>
2023-04-10 21:59:23 -0400

    Canditate solution and eulerian tour

--- src/main/java/edu/northeastern/info6205/tspsolver/service/impl/TSPSolverServiceImpl.java
+++ src/main/java/edu/northeastern/info6205/tspsolver/service/impl/TSPSolverServiceImpl.java
@@ -10,6 +10,7 @@ import java.util.Set;
 import java.util.Stack;
 import java.util.function.Function;
 
+import edu.northeastern.info6205.tspsolver.harshil.FluerysAlgorithm;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -45,6 +46,11 @@ public class TSPSolverServiceImpl implements TSPSolverService {
 				startingPointIndex);
 		
 		Runnable runnable = () -> {
+			Map<Integer, Point> pointMap = new HashMap<>();
+			for(Point point: points) {
+				pointMap.put(Integer.parseInt(point.getId()), point);
+			}
+
 			PrimsMST primsMST = new PrimsMST(points);
 			primsMST.solve();
 			Edge[] edges = primsMST.getMst();
@@ -67,52 +73,56 @@ public class TSPSolverServiceImpl implements TSPSolverService {
                     .mapToDouble(Edge::getDistance)
                     .sum();
 			LOGGER.trace("totalCost of matching edges: {}", totalCost);
-			
-			Map<Point, List<Edge>> multigraph = new HashMap<>();
-			
-			for (Point point : points) {
-				multigraph.put(point, new ArrayList<>());
+
+			List<Edge> multigraph = new ArrayList<>();
+			multigraph.addAll(mstEdgeList);
+			multigraph.addAll(matchingEdges);
+
+			FluerysAlgorithm eulerianCircuit =  new FluerysAlgorithm(multigraph.size());
+			for(Edge edge: multigraph) {
+				int sourceIndex = Integer.parseInt(edge.from.getId());
+				int destinationIndex = Integer.parseInt(edge.to.getId());
+
+				eulerianCircuit.addEdge(sourceIndex, destinationIndex);
 			}
-			
-			for (Edge edge : mstEdgeList) {
-				Point source = edge.from;
-			    Point target = edge.to;
-			    
-			    multigraph.get(source).add(new Edge(source, target));
-			    multigraph.get(target).add(new Edge(target, source));
+
+			eulerianCircuit.printEulerTour();
+			List<int[]> circuit = eulerianCircuit.getResult();
+			List<Edge> eulerianTour = new ArrayList<>();
+
+
+			mapService.publishClearMap();
+			for(int[] edge: circuit) {
+				int sourceIndex = edge[0];
+				int destinationIndex = edge[1];
+
+				Point source = pointMap.get(sourceIndex);
+				Point destination = pointMap.get(destinationIndex);
+
+				Edge path = new Edge(source, destination);
+				eulerianTour.add(path);
+
+
 			}
-			
-			for (Edge edge : matchingEdges) {
-				Point source = edge.from;
-			    Point target = edge.to;
-			    
-			    multigraph.get(source).add(new Edge(source, target));
-			    multigraph.get(target).add(new Edge(target, source));
-			}
-			
-			List<Edge> eulerianTour = findEulerianTour(multigraph);
-			
+
+
+			mapService.publishAddPointsAndFitBound(points);
+
 			List<Point> hamiltonianCycle = new ArrayList<>();
 			Set<Point> visited = new HashSet<>();
 			for (Edge edge : eulerianTour) {
 				Point source = edge.from;
 				Point target = edge.to;
-				
+
 				if (visited.add(source)) {
 					hamiltonianCycle.add(source);
 				}
-				
+
 				if (visited.add(target)) {
 					hamiltonianCycle.add(target);
 				}
 			}
-			
-			LOGGER.trace("hamiltonianCycle size: {}", hamiltonianCycle.size());
-			
-			mapService.publishClearMap();
-			
-			mapService.publishAddPointsAndFitBound(hamiltonianCycle);
-			
+
 			List<Edge> initialTSPTour = new ArrayList<>();
 			for (int i = 0; i < hamiltonianCycle.size() - 1; i++) {
 				Point source = hamiltonianCycle.get(i);
@@ -120,30 +130,89 @@ public class TSPSolverServiceImpl implements TSPSolverService {
 				Edge edge = new Edge(source, destination);
 				initialTSPTour.add(edge);
 			}
-			
+
 			// To complete last edge of cycle where we come back to the source
 			Point source = hamiltonianCycle.get(hamiltonianCycle.size()  - 1);
 			Point destination = hamiltonianCycle.get(0);
 			Edge lastEdge = new Edge(source, destination);
 			initialTSPTour.add(lastEdge);
-			
-			LOGGER.trace("initial tspTour size: {}", initialTSPTour.size());
 
+//			Map<Point, List<Edge>> multigraph = new HashMap<>();
+//
+//			for (Point point : points) {
+//				multigraph.put(point, new ArrayList<>());
+//			}
+//
+//			for (Edge edge : mstEdgeList) {
+//				Point source = edge.from;
+//			    Point target = edge.to;
+//
+//			    multigraph.get(source).add(new Edge(source, target));
+//			    multigraph.get(target).add(new Edge(target, source));
+//			}
+//
+//			for (Edge edge : matchingEdges) {
+//				Point source = edge.from;
+//			    Point target = edge.to;
+//
+//			    multigraph.get(source).add(new Edge(source, target));
+//			    multigraph.get(target).add(new Edge(target, source));
+//			}
+//
+//			List<Edge> eulerianTour = findEulerianTour(multigraph);
+//
+//			List<Point> hamiltonianCycle = new ArrayList<>();
+//			Set<Point> visited = new HashSet<>();
+//			for (Edge edge : eulerianTour) {
+//				Point source = edge.from;
+//				Point target = edge.to;
+//
+//				if (visited.add(source)) {
+//					hamiltonianCycle.add(source);
+//				}
+//
+//				if (visited.add(target)) {
+//					hamiltonianCycle.add(target);
+//				}
+//			}
+//
+//			LOGGER.trace("hamiltonianCycle size: {}", hamiltonianCycle.size());
+//
+//			mapService.publishClearMap();
+//
+//			mapService.publishAddPointsAndFitBound(hamiltonianCycle);
+//
+//			List<Edge> initialTSPTour = new ArrayList<>();
+//			for (int i = 0; i < hamiltonianCycle.size() - 1; i++) {
+//				Point source = hamiltonianCycle.get(i);
+//				Point destination = hamiltonianCycle.get(i + 1);
+//				Edge edge = new Edge(source, destination);
+//				initialTSPTour.add(edge);
+//			}
+//
+//			// To complete last edge of cycle where we come back to the source
+//			Point source = hamiltonianCycle.get(hamiltonianCycle.size()  - 1);
+//			Point destination = hamiltonianCycle.get(0);
+//			Edge lastEdge = new Edge(source, destination);
+//			initialTSPTour.add(lastEdge);
+//
+//			LOGGER.trace("initial tspTour size: {}", initialTSPTour.size());
+//
 			double totalTSPCost = getTourDistance(initialTSPTour);
 			LOGGER.trace("totalCost of initial TSP Tour: {}", totalTSPCost);
-			
-			for (Edge edge : initialTSPTour) {
-				mapService.publishDrawEdge(edge);
-			}
-			
+
 			List<Edge> improved2OptTSPTour = improve2OPT(initialTSPTour);
 			mapService.publishClearMap();
 			for (Edge edge : improved2OptTSPTour) {
 				mapService.publishDrawEdge(edge);
 			}
-			
+
 			double improved2OptTSPTourCost = getTourDistance(improved2OptTSPTour);
 			LOGGER.trace("totalCost of improved2OptTSPTour: {}", improved2OptTSPTourCost);
+
+			double goldenRatio = improved2OptTSPTourCost / mstCost;
+			LOGGER.trace("goldenRatio: {}", goldenRatio);
+
 		};
 		
 		/*
