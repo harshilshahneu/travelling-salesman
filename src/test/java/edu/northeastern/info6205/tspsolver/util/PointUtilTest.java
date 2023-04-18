b723cf681f1c2a5ca3e090ae882de1e64d0cfe38
Dinesh Shekhawat <shekhawat.d@northeastern.edu>
2023-04-18 16:53:49 -0400

    Add test cases for PointUtilTest

--- src/test/java/edu/northeastern/info6205/tspsolver/util/PointUtilTest.java
+++ src/test/java/edu/northeastern/info6205/tspsolver/util/PointUtilTest.java
@@ -1,14 +1,41 @@
 package edu.northeastern.info6205.tspsolver.util;
 
-import org.junit.Test;
-import org.junit.jupiter.api.Disabled;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import edu.northeastern.info6205.tspsolver.constant.Constant;
+import edu.northeastern.info6205.tspsolver.model.Point;
+import edu.northeastern.info6205.tspsolver.service.CSVParserService;
+import edu.northeastern.info6205.tspsolver.service.impl.CSVParserServiceImpl;
 
 public class PointUtilTest {
 
 	@Test
-    @Disabled("This test case needs to be implemented")
-    public void test() {
-    	
-    }
+	public void nullTest() {
+		assertThrows(NullPointerException.class, () -> {
+			PointUtil.getTotalCost(null);
+		});
+	}
+	
+	@Test
+	public void emptyListZeroCostTest() {
+		double cost = PointUtil.getTotalCost(Collections.emptyList());
+		assertEquals(cost, 0);
+	}
+	
+	@Test
+	public void smallDataTest() {
+		CSVParserService csvParserService = CSVParserServiceImpl.getInstance();
+		List<Point> points = csvParserService.parsePoints(Constant.TEST_DATA_FILE_SMALL);
+		
+		double cost = PointUtil.getTotalCost(points);
+		double expectedCost = 71597.66003606733;
+		assertEquals(cost, expectedCost);
+	}
 	
 }
