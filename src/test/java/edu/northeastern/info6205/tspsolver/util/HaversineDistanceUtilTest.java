7d409b2168c69cf201c083489547262e24e5de6d
Dinesh Shekhawat <shekhawat.d@northeastern.edu>
2023-04-18 00:58:58 -0400

    HaversineDistanceUtilTest code cleanup

--- src/test/java/edu/northeastern/info6205/tspsolver/util/HaversineDistanceUtilTest.java
+++ src/test/java/edu/northeastern/info6205/tspsolver/util/HaversineDistanceUtilTest.java
@@ -4,94 +4,56 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
+import edu.northeastern.info6205.tspsolver.constant.Constant;
 import edu.northeastern.info6205.tspsolver.model.Point;
 
 public class HaversineDistanceUtilTest {
-    private static final Logger LOGGER = LoggerFactory.getLogger(HaversineDistanceUtilTest.class);
 
     @Test
-    public void haversineBetweenSamePoints() {
-        LOGGER.info("haversineBetweenSamePoints()");
-        Point point1 = new Point("a",42.358056, -71.063611); // Boston
-        Point point2 = new Point("b",42.358056, -71.063611); // Boston
-
-        double distance = HaversineDistanceUtil.haversine(point1, point2);
+    public void distanceBetweenSamePointsTest() {
+        Point boston = new Point(Constant.APP_NAME, 42.358056, -71.063611);
+        Point bostonDuplicate = new Point(Constant.APP_NAME, 42.358056, -71.063611);
+        double distance = HaversineDistanceUtil.haversine(boston, bostonDuplicate);
         assertEquals(0, distance);
     }
 
     @Test
-    public void haversineBetweenDifferentPoints() {
-        LOGGER.trace("haversineBetweenDifferentPoints()");
-        Point point1 = new Point("a",40.712776, -74.005974); // New York City
-        Point point2 = new Point("b",42.360081, -71.058884); // Los Angeles
+    public void distanceBetweenNewYorkAndLosAngelesTest() {
+        Point newYork = new Point(Constant.APP_NAME, 40.712776, -74.005974);
+        Point losAngeles = new Point(Constant.APP_NAME, 42.360081, -71.058884);
         double expectedDistance = 306108.2254187825;
-        double distance = HaversineDistanceUtil.haversine(point1, point2);
+        double distance = HaversineDistanceUtil.haversine(newYork, losAngeles);
         assertEquals(expectedDistance, distance);
     }
-
+    
     @Test
-    public void nullPointCheck() {
-        LOGGER.trace("nullPointCheck()");
-        Point point1 = null;
-        Point point2 = new Point("b",42.360081, -71.058884); // Los Angeles
-
-        assertThrows(NullPointerException.class, () -> {
-            HaversineDistanceUtil.haversine(point1, point2);
-        });
+    public void distanceBetweenMumbaiAndDelhiTest() {
+        Point mumbai = new Point(Constant.APP_NAME, 19.0760, 72.8777);
+        Point delhi = new Point(Constant.APP_NAME, 28.7041, 77.1025);
+        double expectedDistance = 1153241.2912502035;
+        double distance = HaversineDistanceUtil.haversine(mumbai, delhi);
+        assertEquals(expectedDistance, distance);
     }
-
+    
     @Test
-    public void nullPointCheck2() {
-        LOGGER.trace("nullPointCheck2()");
-        Point point1 = new Point("b",42.360081, -71.058884); // New York City
-        Point point2 = null;
-
-        assertThrows(NullPointerException.class, () -> {
-            HaversineDistanceUtil.haversine(point1, point2);
-        });
-    }
-
-    @Test
-    public void nullPointCheck3() {
-        LOGGER.trace("nullPointCheck3()");
-        Point point1 = null;
-        Point point2 = null;
-
-        assertThrows(NullPointerException.class, () -> {
-            HaversineDistanceUtil.haversine(point1, point2);
-        });
-    }
-
-    @Test
-    public void test5() {
-        LOGGER.trace("test5()");
-        Point northPole = new Point("a", 90.0, 0.0);
-        Point southPole = new Point("a", -90.0, 0.0);
+    public void distanceBetweenNorthPoleSouthPoleTest() {
+        Point northPole = new Point(Constant.APP_NAME, 90.0, 0.0);
+        Point southPole = new Point(Constant.APP_NAME, -90.0, 0.0);
         double distanceInMeters = HaversineDistanceUtil.haversine(northPole, southPole);
-        double expectedDistance = 2.001508679602057E7; // approximate value based on online calculators
+        
+        // approximate value based on online calculators
+        double expectedDistance = 2.001508679602057E7;
         assertEquals(expectedDistance, distanceInMeters);
     }
 
     @Test
-    public void haversineBetweenDifferentPoints2() {
-        LOGGER.trace("haversineBetweenDifferentPoints2()");
-        Point point1 = new Point("a",19.0760, 72.8777); // Mumbai
-        Point point2 = new Point("b",28.7041, 77.1025); // Delhi
-        double expectedDistance = 1153241.2912502035;
-        double distance = HaversineDistanceUtil.haversine(point1, point2);
-        assertEquals(expectedDistance, distance);
-    }
-
-    @Test
-    public void haversineBetweenSamePoints2() {
-        LOGGER.trace("haversineBetweenSamePoints2()");
-        Point point1 = new Point("a",1, 180.0);
-        Point point2 = new Point("b",1, -180.0);
+    public void distanceBetweenExtremeLongitudeTest() {
+        Point point1 = new Point(Constant.APP_NAME, 1, 180.0);
+        Point point2 = new Point(Constant.APP_NAME, 1, -180.0);
         double expectedDistance = 0;
-        double distance = (float)HaversineDistanceUtil.haversine(point1, point2);
+        double distance = HaversineDistanceUtil.haversine(point1, point2);
+        
         /* In terms of location on the Earth's surface,
         -180 degrees longitude is equivalent to 180 degrees longitude.
         If we don't use delta, the actual value of distance is 1.5602072966913738E-9
@@ -100,5 +62,34 @@ public class HaversineDistanceUtilTest {
         assertEquals(expectedDistance, distance, 1E-8);
     }
 
+    @Test
+    public void nullDestinationPointTest() {
+        Point nullPoint = null;
+        Point losAngeles = new Point(Constant.APP_NAME, 42.360081, -71.058884);
+
+        assertThrows(NullPointerException.class, () -> {
+            HaversineDistanceUtil.haversine(nullPoint, losAngeles);
+        });
+    }
+
+    @Test
+    public void nullSourcePointTest() {
+        Point newYork = new Point(Constant.APP_NAME, 42.360081, -71.058884);
+        Point nullPoint = null;
+
+        assertThrows(NullPointerException.class, () -> {
+            HaversineDistanceUtil.haversine(newYork, nullPoint);
+        });
+    }
+
+    @Test
+    public void nullBothPointsTest() {
+        Point firstNullPoint = null;
+        Point secondNullPoint = null;
+
+        assertThrows(NullPointerException.class, () -> {
+            HaversineDistanceUtil.haversine(firstNullPoint, secondNullPoint);
+        });
+    }
 
 }
