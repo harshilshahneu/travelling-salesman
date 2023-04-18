41767a79f65173542e99f3ef71c9f5c00cd6c569
Dinesh Shekhawat <shekhawat.d@northeastern.edu>
2023-04-18 02:19:19 -0400

    Remove disable from Point test and change test log level to ERROR

--- src/test/java/edu/northeastern/info6205/tspsolver/model/PointTest.java
+++ src/test/java/edu/northeastern/info6205/tspsolver/model/PointTest.java
@@ -4,27 +4,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import edu.northeastern.info6205.tspsolver.constant.Constant;
 
 public class PointTest {
     private static final Logger LOGGER = LoggerFactory.getLogger(PointTest.class);
 
     @Test
-    @Disabled("This test case needs to be implemented")
-    public void test() {
-
-    }
-    
-    @Test
     public void invalidCoordinates() {
         LOGGER.trace("invalidCoordinates()");
 
         assertThrows(IllegalArgumentException.class, () -> {
-            new Point("", 100.0, 0.0);
-            new Point("", 40.712776, 40.0);
+            new Point(Constant.BLANK_STRING, 100.0, 0.0);
+            new Point(Constant.BLANK_STRING, 40.712776, 40.0);
         });
     }
 
@@ -33,8 +28,8 @@ public class PointTest {
         LOGGER.trace("invalidCoordinates2()");
 
         assertThrows(IllegalArgumentException.class, () -> {
-            new Point("", 0.0, 181.0);
-            new Point("", 1.0, 40.0);
+            new Point(Constant.BLANK_STRING, 0.0, 181.0);
+            new Point(Constant.BLANK_STRING, 1.0, 40.0);
         });
     }
 
@@ -43,8 +38,8 @@ public class PointTest {
         LOGGER.trace("invalidCoordinates3()");
 
         assertThrows(IllegalArgumentException.class, () -> {
-            new Point("", 0.0, 18.0);
-            new Point("", -91.0, 40.0);
+            new Point(Constant.BLANK_STRING, 0.0, 18.0);
+            new Point(Constant.BLANK_STRING, -91.0, 40.0);
         });
     }
 
@@ -53,24 +48,24 @@ public class PointTest {
         LOGGER.trace("invalidCoordinates4()");
 
         assertThrows(IllegalArgumentException.class, () -> {
-            new Point("", 0.0, 18.0);
-            new Point("", -9.0, -181.0);
+            new Point(Constant.BLANK_STRING, 0.0, 18.0);
+            new Point(Constant.BLANK_STRING, -9.0, -181.0);
         });
     }
 
     @Test
     public void sameCoordinates() {
         LOGGER.trace("sameCoordinates()");
-        Point point1 = new Point("a", 1.0, 18.0);
-        Point point2 = new Point("a", 1.0, 18.0);
+        Point point1 = new Point(Constant.APP_NAME, 1.0, 18.0);
+        Point point2 = new Point(Constant.APP_NAME, 1.0, 18.0);
         assertEquals(point1, point2);
     }
 
     @Test
     public void sameCoordinatesDifferentId() {
         LOGGER.trace("sameCoordinates2()");
-        Point point1 = new Point("a", 1.0, 18.0);
-        Point point2 = new Point("b", 1.0, 18.0);
+        Point point1 = new Point(Constant.APP_NAME, 1.0, 18.0);
+        Point point2 = new Point(Constant.DASH, 1.0, 18.0);
         assertNotEquals(point1, point2);
     }
 
