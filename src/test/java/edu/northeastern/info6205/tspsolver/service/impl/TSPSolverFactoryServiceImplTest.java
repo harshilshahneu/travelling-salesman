62dfed67d9ab38e61dc3a0f0f64af2c3ca8cf3eb
Dinesh Shekhawat <shekhawat.d@northeastern.edu>
2023-04-18 20:28:33 -0400

    Add test cases for TSPSolverFactoryServiceTest & TSPSolverFactoryServiceImplTest

--- src/test/java/edu/northeastern/info6205/tspsolver/service/impl/TSPSolverFactoryServiceImplTest.java
+++ src/test/java/edu/northeastern/info6205/tspsolver/service/impl/TSPSolverFactoryServiceImplTest.java
@@ -1,10 +1,14 @@
 package edu.northeastern.info6205.tspsolver.service.impl;
 
+import edu.northeastern.info6205.tspsolver.constant.Constant;
 import edu.northeastern.info6205.tspsolver.service.TSPSolverFactoryService;
-import org.junit.Test;
+import edu.northeastern.info6205.tspsolver.service.TSPSolverService;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import org.junit.jupiter.api.Test;
 
 public class TSPSolverFactoryServiceImplTest {
 	
@@ -19,6 +23,78 @@ public class TSPSolverFactoryServiceImplTest {
         TSPSolverFactoryService firstInstance = TSPSolverFactoryServiceImpl.getInstance();
         TSPSolverFactoryService secondInstance = TSPSolverFactoryServiceImpl.getInstance();
         assertEquals(firstInstance, secondInstance);
+    }
+    
+    @Test
+    public void nullIdentifierTest() {
+    	TSPSolverFactoryService service = TSPSolverFactoryServiceImpl.getInstance();
+    	assertNull(service.getService(null));
+    }
+    
+    @Test
+    public void invalidIdentifierTest() {
+    	TSPSolverFactoryService service = TSPSolverFactoryServiceImpl.getInstance();
+    	assertNull(service.getService(Constant.BLANK_STRING));
+    }
+    
+    @Test
+    public void christofidesTest() {
+    	implementationTest(
+    			Constant.KEY_IDENTIFIER_CHRISTOFIDES, 
+    			Constant.NAME_CHRISTOFIDES, 
+    			TSPChristofidesSolverServiceImpl.class);
+    }
+    
+    @Test
+    public void twoOptTest() {
+    	implementationTest(
+    			Constant.KEY_IDENTIFIER_RANDOM_TWO_OPT, 
+    			Constant.NAME_RANDOM_TWO_OPT, 
+    			TSPRandomTwoOptSolverServiceImpl.class);
+    }
+    
+    @Test
+    public void threeOptTest() {
+    	implementationTest(
+    			Constant.KEY_IDENTIFIER_RANDOM_THREE_OPT, 
+    			Constant.NAME_RANDOM_THREE_OPT, 
+    			TSPRandomThreeOptSolverServiceImpl.class);
+    }
+    
+    @Test
+    public void simulatedAnnealingTest() {
+    	implementationTest(
+    			Constant.KEY_IDENTIFIER_SIMULATED_ANNEALING, 
+    			Constant.NAME_SIMULATED_ANNEALING, 
+    			TSPSimulatedAnnealingSolverServiceImpl.class);
+    }
+    
+    @Test
+    public void antColonyTest() {
+    	implementationTest(
+    			Constant.KEY_IDENTIFIER_ANT_COLONY, 
+    			Constant.NAME_ANT_COLONY, 
+    			TSPAntColonyOptimzationSolverServiceImpl.class);
+    }
+    
+    @Test
+    public void antColonyThreeOptTest() {
+    	implementationTest(
+    			Constant.KEY_IDENTIFIER_ANT_COLONY_THREE_OPT, 
+    			Constant.NAME_ANT_COLONY_THREE_OPT, 
+    			TSPACOThreeOptSolverServiceImpl.class);
+    }
+    
+    private void implementationTest(
+    		String keyIdentifier,
+    		String name,
+    		Class<? extends TSPSolverService> serviceClass) {
+    	TSPSolverFactoryService service = TSPSolverFactoryServiceImpl.getInstance();
+    	TSPSolverService solverService = service.getService(keyIdentifier);
+    	
+    	assertEquals(solverService.getClass(), serviceClass);
+    	assertEquals(solverService.getName(), name);
+    	assertEquals(solverService.getKeyIdentifier(), keyIdentifier);
     }
 
 }
